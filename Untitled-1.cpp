ba64693eaed368d5b8b79ad1cdf1c5e26eb064ff
Leticia Pinheiro <leticiapinheiro3137@gmail.com>
2021-10-01 16:28:17 -0300

    Inicial

---
+++ Untitled-1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+    cout << "Hello World" << endl;
+    // esse cout funciona similarmente ao printf em c
+    
+    return 0;
+}
